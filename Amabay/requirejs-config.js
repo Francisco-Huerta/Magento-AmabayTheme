2a53b089f423a61e704252f9e5b04d5e7363b0c8
Francisco Huerta Yumha <fjhuerta.y@gmail.com>
2024-02-13 09:43:52 -0300

    Feat: Bid js module added, added bidding modal to catalog product view

--- Amabay/requirejs-config.js
+++ Amabay/requirejs-config.js
@@ -1,10 +1,11 @@
 var config = {
     deps: [
-        'js/region_setter'
+        'js/region_setter',
     ],
     map: {
         '*': {
-            'Webkul_Marketplace/js/form-button-action': 'Bidder_Amabay/js/form-button-action'
+            'Webkul_Marketplace/js/form-button-action': 'Bidder_Amabay/js/form-button-action',
+            auction: 'js/auction',
         }
     }
 };
